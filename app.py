5a6d52ad416b22b96d7cca348e1e073356048cb7
Anas <com.anas.idms.appleid.prd.001784-08-5d674918-865b-4553-9bf0-c2326b981e34@AB-Macbook.local>
2019-10-02 00:14:10 -0700

    verified heroku account

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import os
 
 
 client = MongoClient()
@@ -64,5 +65,12 @@ def playlists_delete(playlist_id):
     return redirect(url_for('playlists_index'))
 
 
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
+playlists = db.playlists
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
